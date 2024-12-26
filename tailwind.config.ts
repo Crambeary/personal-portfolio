26793b31615c5270838a0a5e9ae0d891d235f115
Marc <marctotaro94@gmail.com>
2024-12-26 13:34:01 -0600

    tailwind supported

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import tailwindcssMotion from 'tailwindcss-motion';
 
 module.exports = {
   content: [
@@ -14,5 +15,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssMotion],
 } satisfies Config;
