99996fb2f669ade1cb3a2830e74bd589a5d33ee9
Crambeary <3926951+Crambeary@users.noreply.github.com>
2024-12-28 08:38:28 -0600

    feat: Dark Mode Toggle with new brand colors (#3)
    * enable shadcn/ui and next-themes
    
    * imported some shadcn/ui components
    
    * theme toggling
    
    * added themes with brown
    
    * default dark mode
    
    * skeleton load theme toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { ThemeToggle } from '@/components/theme-toggle';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
       <h1>Home</h1>
+      <ThemeToggle />
       <Link href="/about">
         <button>About</button>
       </Link>
---
+++ components/theme-toggle.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useTheme } from "next-themes"
+import { Sun, Moon } from 'lucide-react'
+import { Button } from "@/components/ui/button"
+import { useEffect, useState } from "react"
+import { Skeleton } from "./ui/skeleton"
+
+export function ThemeToggle() {
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return (
+      <Skeleton className="h-10 w-10" />
+    )
+  }
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      aria-label={theme === "light" ? "Switch to dark mode" : "Switch to light mode"}
+    >
+      {theme === "light" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      ) : (
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      )}
+    </Button>
+  )
+}
+
