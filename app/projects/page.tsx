59565495575d52ca395f20d0636dc68f665dda5c
Marc <marctotaro94@gmail.com>
2024-12-28 16:46:05 -0600

    fix: added remaining pages and brand name

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+export default function Projects() {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <div className="flex h-screen w-full flex-col items-center justify-center">
+        <h1>Projects</h1>
+      </div>
+    </div>
+  );
+}
