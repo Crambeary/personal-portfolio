7dea87b479393e2809942364aa8eab804b83369d
Marc Totaro <marctotaro94@gmail.com>
2025-01-02 07:48:17 -0600

    highlight h1 text

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,17 @@ export default function Home() {
     <>
       <div className="flex w-full flex-col justify-center">
         <div className="justify-center text-left my-4 sm:my-64 mx-4 sm:ml-24">
-          <h1 className="text-3xl sm:text-5xl font-bold motion-preset-blur-right bg-gradient-to-r from-black dark:from-primary dark:to-gray-400 bg-clip-text text-transparent">
-            Crafting Intuitive Front-End Experiences with React and Tailwind
+          <h1 className="text-3xl sm:text-5xl my-32 sm:mb-64 leading-relaxed sm:pb-4 font-bold motion-preset-blur-right bg-gradient-to-r from-black dark:from-primary dark:to-gray-400 bg-clip-text text-transparent">
+            Crafting Intuitive Front-End <span className="text-border font-serif italic">Experiences</span>
           </h1>
           <div>
             <h2 className="text-xl sm:text-2xl mt-4 font-bold motion-delay-200 motion-preset-blur-right bg-gradient-to-r from-black dark:from-primary dark:to-gray-400 bg-clip-text text-transparent">
               <span className="text-2xl sm:text-3xl text-border font-serif font-normal">
                 I&apos;m Marc
               </span>
-              , a Front-End Developer passionate about building engaging web
+              , a Front-End Developer <span>passionate about building engaging web
               experiences. Currently trasitioning from software testing to web
-              development.
+              development.</span>
             </h2>
           </div>
           <Button
